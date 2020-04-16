e8383a1a1be3a0aabf50b2b63adbe350dc045638
jen <jengkarlong@gmail.com>
2020-04-16 18:08:23 +0800

    added template ui..

--- src/components/TemplateContent.tsx
+++ src/components/TemplateContent.tsx
@@ -25,7 +25,7 @@ const DisplayTypes = (props: any) => {
     for (const type of displayTypes) {
         let isActive = type.title === props.active
         types.push(
-            <div key={displayTypes.indexOf(type)} className={`bot-type ${isActive && 'active'} ${type.class} border border-solid rounded-md`} onClick={() => props.onClick(type.title)}>
+            <div key={displayTypes.indexOf(type)} className={`bot-type ${isActive && 'active'} ${type.class} border border-solid rounded-md mx-4`} onClick={() => props.onClick(type.title)}>
                 <img src={type.img} alt="" width="100" />
                 <span className="bot-type-title">
                     <span className="bot-type-title">{type.title}</span>
@@ -43,7 +43,7 @@ interface State {
 class TemplateContent extends React.Component<any> {
 
     state: State = {
-        active: ""
+        active: "FULLPAGE"
     }
 
     handleBotTypeClick(type: string) {
@@ -56,11 +56,24 @@ class TemplateContent extends React.Component<any> {
         return (
             <div className="template-content">
                 <div className="bot-styles">
-                    <span className="">1. Choose your chatpack display type.</span>
-                    <div className="bot-types flex w-full">
-                        <DisplayTypes onClick={this.handleBotTypeClick.bind(this)} active={this.state.active} />
+                    <div className="builder-content-list">
+                        <span>1. Choose your chatpack display type</span>
+                        <div className="bot-types flex w-full my-3 ml-5">
+                            <DisplayTypes onClick={this.handleBotTypeClick.bind(this)} active={this.state.active} />
+                        </div>
                     </div>
 
+                    <div className="builder-content-list">
+                        <span>2. Design template</span>
+                        <div className="bot-templates flex flex-wrap w-full my-3 ml-5">
+                            <div className="box-template bg-orange-300 m-4"></div>
+                            <div className="box-template bg-gray-300 m-4"></div>
+                            <div className="box-template bg-blue-300 m-4"></div>
+                            <div className="box-template bg-green-300 m-4"></div>
+                            <div className="box-template bg-red-300 m-4"></div>
+                            <div className="box-template bg-yellow-300 m-4"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
