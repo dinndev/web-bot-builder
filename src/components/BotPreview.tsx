a573a77e5db9ef17a486f31b7a8b52352e4fc874
jen <jengkarlong@gmail.com>
2020-04-16 15:27:51 +0800

    bot type option done also clickable

--- src/components/BotPreview.tsx
+++ src/components/BotPreview.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const BotPreview = (props: any) => {
     return (
-        <div className="bot-preview w-full">
-            preview
+        <div className="bot-preview w-full font-bold text-xl bg-gray-200 flex justify-center items-center">
+            <span className="text-gray-500">Bot UI Preview</span>
         </div>
     )
 }
--- src/components/TemplateContent.tsx
+++ src/components/TemplateContent.tsx
@@ -1,9 +1,70 @@
 import React from 'react'
+import Full from '../assets/full.png'
+import Popup from '../assets/popup.png'
+import Embed from '../assets/embed.png'
+const displayTypes = [
+    {
+        title: 'FULLPAGE',
+        class: "bot-type-full",
+        img: Full
+    },
+    {
+        title: 'POPUP',
+        class: "bot-type-popup",
+        img: Popup
+    },
+    {
+        title: 'EMBED',
+        class: "bot-type-embed",
+        img: Embed
+    }
+]
 
-const TemplateContent = () => {
-    return (
-        <div>Template content</div>
-    )
+const DisplayTypes = (props: any) => {
+    let types: any = [];
+    for (const type of displayTypes) {
+        let isActive = type.title === props.active
+        types.push(
+            <div key={displayTypes.indexOf(type)} className={`bot-type ${isActive && 'active'} ${type.class} border border-solid rounded-md`} onClick={() => props.onClick(type.title)}>
+                <img src={type.img} alt="" width="100" />
+                <span className="bot-type-title">
+                    <span className="bot-type-title">{type.title}</span>
+                </span>
+            </div>
+        )
+    }
+
+    return types
+}
+
+interface State {
+    active: string
+}
+class TemplateContent extends React.Component<any> {
+
+    state: State = {
+        active: ""
+    }
+
+    handleBotTypeClick(type: string) {
+        this.setState({
+            active: type
+        })
+    }
+
+    render() {
+        return (
+            <div className="template-content">
+                <div className="bot-styles">
+                    <span className="">1. Choose your chatpack display type.</span>
+                    <div className="bot-types flex w-full">
+                        <DisplayTypes onClick={this.handleBotTypeClick.bind(this)} active={this.state.active} />
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
 }
 
 export default TemplateContent
